7c4f1e461a5aaee6d2b19165454276d40380b514
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 19:03:23 +0700

    Add files via upload

---
+++ Switch Case.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char huruf = 'B';
+	switch(angka){
+		case 'A':
+			cout<<"nilai Anda 100\n";
+			break;
+		case 'B':
+			cout<<"nilai Anda 75-99\n";
+			break;
+		case 'C':
+			cout<<"nilai Anda di bawah 76\n";
+			break;
+		default:
+			cout<<"nilai tidak terdaftar\n";
+	}
+}
